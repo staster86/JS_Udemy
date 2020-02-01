2821272ef0442ddbd09324193afe352333b908e4
Stas <staster86@gmail.com>
2020-02-01 13:48:02 +0400

    Это наш первый коммит

---
+++ script.js
@@ -0,0 +1,30 @@
+'use strict'
+
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer);
+
+// let answer = prompt("Вам 18 лет и более?", "Да");
+
+// console.log(typeof(answer));
+
+// console.log("arr" + " - " + "object");
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(++incr); // 11 префиксное
+// console.log(--decr); // 9
+
+// console.log(incr++); // 10 постфиксное
+// console.log(decr--); // 10
+
+// console.log(5%2); // 1 остаток от деления
+
